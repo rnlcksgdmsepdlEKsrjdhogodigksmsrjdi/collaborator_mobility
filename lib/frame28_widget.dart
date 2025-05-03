7e49a0e055b324857020ee0c3c270ba594e4e1d1
rasol <poloa0709@gmail.com>
2025-05-03 17:06:54 +0900

    위치 팝업 구현

--- lib/frame28_widget.dart
+++ lib/frame28_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../widgets/custom_popup.dart';
 
 class Frame28Widget extends StatelessWidget {
   const Frame28Widget({super.key});
@@ -7,44 +9,42 @@ class Frame28Widget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+        borderRadius: BorderRadius.vertical(top: Radius.circular(15.r)),
         boxShadow: [
           BoxShadow(
-            color: Color.fromRGBO(217, 217, 217, 0.6),
-            offset: Offset(0, 4),
-            blurRadius: 13,
+            color: const Color.fromRGBO(217, 217, 217, 0.6),
+            offset: Offset(0, 4.h),
+            blurRadius: 13.r,
           ),
         ],
       ),
       child: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 13, horizontal: 21),
+          padding: EdgeInsets.symmetric(vertical: 13.h, horizontal: 21.w),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Center(
                 child: Container(
-                  width: 54,
-                  height: 5,
+                  width: 54.w,
+                  height: 5.h,
                   decoration: BoxDecoration(
                     color: const Color.fromRGBO(217, 217, 217, 1),
-                    borderRadius: BorderRadius.circular(100),
+                    borderRadius: BorderRadius.circular(100.r),
                   ),
                 ),
               ),
-              const SizedBox(height: 35),
-              
-              const SizedBox(height: 30),
-              const Divider(color: Color.fromRGBO(245, 245, 245, 1), thickness: 9),
+              SizedBox(height: 35.h),
+              SizedBox(height: 30.h),
+              Divider(color: const Color.fromRGBO(245, 245, 245, 1), thickness: 9.h),
+              SizedBox(height: 20.h),
 
-              const SizedBox(height: 20),
-            
               // 목적지 목록들
-              buildPlaceItem('assets/images/place1.svg', '조선대학교 해오름관'),
-              buildPlaceItem('assets/images/place2.svg', '조선대학교 중앙도서관'),
-              buildPlaceItem('assets/images/place3.svg', '조선대학교 IT융합대학'),
+              buildPlaceItem(context, 'assets/images/place1.svg', '조선대학교 해오름관'),
+              buildPlaceItem(context, 'assets/images/place2.svg', '조선대학교 중앙도서관'),
+              buildPlaceItem(context, 'assets/images/place3.svg', '조선대학교 IT융합대학'),
             ],
           ),
         ),
@@ -52,38 +52,65 @@ class Frame28Widget extends StatelessWidget {
     );
   }
 
-    Widget buildPlaceItem(String iconPath, String label) {
+  Widget buildPlaceItem(BuildContext context, String iconPath, String label) {
     return Column(
       children: [
         Row(
           children: [
-            SvgPicture.asset(iconPath),
-            const SizedBox(width: 12),
-            Expanded(
-              child: Text(
-                label,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'Paperlogy',
-                  fontSize: 20,
-                  height: 1.0,
-                ),
+            GestureDetector(
+              onTap: () {
+                showGeneralDialog(
+                  context: context,
+                  barrierDismissible: true,
+                  barrierLabel: "팝업",
+                  barrierColor: const Color.fromRGBO(0, 0, 0, 0.4),
+                  transitionDuration: const Duration(milliseconds: 300),
+                  pageBuilder: (context, anim1, anim2) {
+                    return const SizedBox();
+                  },
+                  transitionBuilder: (context, anim1, anim2, child) {
+                    return FadeTransition(
+                      opacity: anim1,
+                      child: CustomPopup(label: label),
+                    );
+                  },
+                );
+              },
+              child: Row(
+                children: [
+                  SvgPicture.asset(
+                    iconPath,
+                    width: 24.w,
+                    height: 24.h,
+                  ),
+                  SizedBox(width: 12.w),
+                  Text(
+                    label,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Paperlogy',
+                      fontSize: 20.sp,
+                      height: 1.0,
+                    ),
+                  ),
+                ],
               ),
             ),
+            const Spacer(),
             Container(
-              width: 76,
-              height: 33,
+              width: 76.w,
+              height: 33.h,
               decoration: BoxDecoration(
                 color: const Color.fromRGBO(3, 3, 97, 1),
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.circular(20.r),
               ),
               alignment: Alignment.center,
-              child: const Text(
+              child: Text(
                 '예약하기',
                 style: TextStyle(
                   color: Colors.white,
                   fontFamily: 'Paperlogy',
-                  fontSize: 15,
+                  fontSize: 15.sp,
                   letterSpacing: -0.5,
                   height: 1.33,
                 ),
@@ -91,14 +118,14 @@ class Frame28Widget extends StatelessWidget {
             ),
           ],
         ),
-        const SizedBox(height: 20),
-        const Divider(
-          thickness: 1,
-          color: Color(0xFFD9D9D9),
+        SizedBox(height: 20.h),
+        Divider(
+          thickness: 1.h,
+          color: const Color(0xFFD9D9D9),
           indent: 0,
           endIndent: 0,
         ),
-        const SizedBox(height: 20),
+        SizedBox(height: 20.h),
       ],
     );
   }
---
+++ lib/widgets/custom_popup.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomPopup extends StatefulWidget {
+  final String label;
+
+  const CustomPopup({super.key, required this.label});
+
+  @override
+  State<CustomPopup> createState() => _CustomPopupState();
+}
+
+class _CustomPopupState extends State<CustomPopup>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
+  late Animation<double> _scaleAnimation;
+
+  final Map<String, Map<String, String>> labelData = {
+    '조선대학교 해오름관': {
+      'name': '조선대학교 해오름관',
+      'address': '광주 동구 조선대5길 31',
+      'charger': '완속 충전기',
+    },
+    '조선대학교 중앙도서관': {
+      'name': '조선대학교 중앙도서관',
+      'address': '광주 동구 조선대 5길 19',
+      'charger': '완속 충전기',
+    },
+    '조선대학교 IT융합대학': {
+      'name': '조선대학교 IT융합대학',
+      'address': '광주 동구 조선대 5길 65',
+      'charger': '완속 충전기',
+    },
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+    _opacityAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+    _scaleAnimation =
+        Tween<double>(begin: 0.9, end: 1.0).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOutBack,
+    ));
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final data = labelData[widget.label] ??
+        {'name': '알 수 없음', 'address': '-', 'charger': '-'};
+    print('data: $data');
+    return Center(
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: FadeTransition(
+          opacity: _opacityAnimation,
+          child: Container(
+            width: 270.w,
+            height: 242.h,
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20.r),
+              border: Border.all(
+                  color: const Color.fromRGBO(217, 217, 217, 1), width: 1),
+            ),
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  top: 44.h,
+                  left: 0,
+                  child: Row(
+                    children: [
+                      SvgPicture.asset('assets/images/place1.svg',
+                          width: 24.w, height: 24.h),
+                      SizedBox(width: 12.w),
+                      Text(
+                        data['name']!,
+                        style: TextStyle(
+                          fontFamily: 'Paperlogy',
+                          fontSize: 20.sp,
+                          letterSpacing: -0.5,
+                          height: 1,
+                          color: Colors.black,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 88.h,
+                  left: 0,
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 20.w,
+                        height: 20.h,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              top: 1.6.h,
+                              left: 0.8.w,
+                              child: SvgPicture.asset('assets/images/map3.svg'),
+                            ),
+                            Positioned(
+                              top: 1.6.h,
+                              left: 6.6.w,
+                              child: SvgPicture.asset('assets/images/map2.svg'),
+                            ),
+                            Positioned(
+                              top: 5.h,
+                              left: 13.3.w,
+                              child: SvgPicture.asset('assets/images/map1.svg'),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 9.w),
+                      Text(
+                        data['address']!,
+                        style: TextStyle(
+                          fontFamily: 'Paperlogy',
+                          fontSize: 20.sp,
+                          letterSpacing: -0.5,
+                          height: 1,
+                          color: Colors.black,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 132.h,
+                  left: 0,
+                  child: Row(
+                    children: [
+                      SvgPicture.asset('assets/images/charger.svg',
+                          width: 24.w, height: 24.h),
+                      SizedBox(width: 10.w),
+                      Text(
+                        data['charger']!,
+                        style: TextStyle(
+                          fontFamily: 'Paperlogy',
+                          fontSize: 20.sp,
+                          letterSpacing: 0,
+                          height: 1,
+                          color: Colors.black,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 182.h,
+                  left: (270 - 87) / 2.w,
+                  child: GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: Container(
+                      width: 87.w,
+                      height: 43.h,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.r),
+                        color: const Color.fromRGBO(3, 3, 97, 1),
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        '확인',
+                        style: TextStyle(
+                          fontFamily: 'Paperlogy',
+                          fontSize: 22.sp,
+                          letterSpacing: -0.5,
+                          height: 1,
+                          color: Colors.white,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
